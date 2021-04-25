8a1a86c35f1830a317ec1c072084d679adb2b437
Athul.k <athulk121996@gmail.com>
2021-04-25 16:25:52 +0530

    test

--- Personal_Portfolio/settings.py
+++ Personal_Portfolio/settings.py
@@ -127,3 +127,7 @@ MEDIA_URL ='/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+try:
+    from .local_settings import *
+except ImportError:
+    print("No local file. On production")
